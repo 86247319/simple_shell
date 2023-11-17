aec75796e763d3c9597be129a5282ed3ca9d9ef1
ahmed <you@sdfe.com>
2023-11-17 12:57:31 -0800

    as

---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+typedef struct builtin
+{
+	char *type;
+	int (*func)(info_t *);
+} builtin_table;
+/* toem_exits.c */
+char *_strncpy(char *, char *, int);
+char *_strncat(char *, char *, int);
+char *_strchr(char *, char);
+#endif
